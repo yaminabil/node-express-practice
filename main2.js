37f16042fc40740ea86e88e3d1a75ff2f0370c81
bilal younes <94098173+yaminabil@users.noreply.github.com>
2022-11-19 18:27:52 -0500

    finishing my 99 bottles on the wall

--- main2.js
+++ main2.js
@@ -11,6 +11,27 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`<h1>hello ${req.params.name}</h1>`);
 });
 
+app.get("/game", (req, res) => {
+  res.send(
+    "<h1> 99  Bottles of beer on the wall</h1> <a href='/game/98'>take one down, pass it around</a>"
+  );
+});
+app.get("/game/:num", (req, res) => {
+  if (req.params.num > 0) {
+    res.send(
+      `<h1>  ${
+        req.params.num
+      } Bottles of beer on the wall</h1> <a href='/game/${
+        req.params.num - 1
+      }'>take one down, pass it around</a>`
+    );
+  } else {
+    res.send(
+      "<h1> 99  Bottles of beer on the wall</h1> <a href='/game/98'>take one down, pass it around</a>"
+    );
+  }
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
