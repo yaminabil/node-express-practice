953fec1d87ae6a9b04f8ca0fc397c5e8f8dac82e
bilal younes <94098173+yaminabil@users.noreply.github.com>
2022-11-19 18:06:14 -0500

    adding the first route

--- main2.js
+++ main2.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/greeting", (req, res) => {
+  res.send("<h1> hello stranger </h1>");
+});
+app.get("/greeting/:name", (req, res) => {
+  res.send(`<h1>hello ${req.params.name}</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
